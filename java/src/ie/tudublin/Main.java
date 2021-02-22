67e4050259f40b344a6c0f16c64529e9fac49120
bin0-cs <oltean444@gmail.com>
2021-02-22 14:02:47 +0000

    week5 monday class

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,9 +1,7 @@
 package ie.tudublin;
 
-public class Main
-{
-    public void catsAndDogs()
-    {
+public class Main {
+    public void catsAndDogs() {
         System.out.println("Hello world");
 
         Animal misty = new Dog("Misty");
@@ -23,53 +21,47 @@ public class Main
         // What will get printed out??
 
         // 1. topcat, Garfield
-        // 2. garfield, Garfield 
+        // 2. garfield, Garfield
 
         Cat ginger = new Cat("Ginger");
-        while(ginger.getNumLives() > 0)
-        {
+        while (ginger.getNumLives() > 0) {
             ginger.kill();
         }
         ginger.kill();
     }
 
-    public void helloProcessing1()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing1());
+    public void helloProcessing1() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new HelloProcessing1());
     }
 
-    public void helloProcessing2()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing2());
+    public void helloProcessing2() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new HelloProcessing2());
     }
 
-    public void bugZap()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
+    public void bugZap() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new BugZap());
     }
 
-    public void loops()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
+    public void loops() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Loops());
     }
 
-    public void arrays()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
+    public void arrays() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Arrays());
     }
-    public void life()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Life());
+
+    public void life() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Life());
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Main main = new Main();
         main.life();
     }
-} 
+}
